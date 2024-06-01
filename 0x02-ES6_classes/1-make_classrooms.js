5cb7381d5b772270beac6aa6e592beed6c9d17e7
Armolas <armolas06@gmail.com>
2024-06-01 13:35:55 +0100

    added task 1

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,8 @@
+import Classroom from './0-classroom'
+
+export default function initializeRooms(){
+	const rooms = [];
+	rooms.push(new Classroom(19), new Classroom(20), new Classroom(34));
+
+	return rooms;
+}
