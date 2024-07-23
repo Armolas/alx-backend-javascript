d9390d950a3584fedde840dfaf44ccadfb1a3f5e
Armolas <armolas06@gmail.com>
2024-07-23 13:12:52 +0100

    3-read_file_async.js

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,34 @@
+const fs = require('fs/promises');
+
+async function countStudents(path) {
+  try {
+    const data = await fs.readFile(`./${path}`, 'utf-8');
+
+    const lines = data.split('\n').filter((line) => line !== '');
+
+    lines.shift();
+
+    const fieldCount = {};
+
+    for (const line of lines) {
+      const [firstname, , , field] = line.split(',');
+
+      if (!fieldCount[field]) {
+        fieldCount[field] = [];
+      }
+      fieldCount[field].push(firstname);
+    }
+    const numberOfStudents = lines.length;
+    console.log(`Number of students: ${numberOfStudents}`);
+
+    for (const field in fieldCount) {
+      if (field) {
+        console.log(`Number of students in ${field}: ${fieldCount[field].length}. List: ${fieldCount[field].join(', ')}`);
+      }
+    }
+  } catch (error) {
+    throw new Error('Cannot load database');
+  }
+}
+
+module.exports = countStudents;
