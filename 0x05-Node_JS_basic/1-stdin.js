6c853fca76709a15d1fda55b1b8f86f80a3bdb45
Armolas <armolas06@gmail.com>
2024-07-25 00:33:00 +0100

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,7 +4,9 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin.setEncoding('utf8');
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
-  console.log(`Your name is: ${name.trim()}`);
+  if (name) {
+    console.log(`Your name is: ${name.trim()}`);
+  }
 });
 process.stdin.on('end', () => {
   console.log('This important software is now closing');
