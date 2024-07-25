1b2dab34ebe0e591e6d9db4ebeeb2d06515c1841
Armolas <armolas06@gmail.com>
2024-07-25 15:54:56 +0100

    task_6

---
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+function getPaymentTokenFromAPI(success) {
+	return new Promise((resolve, reject) => {
+		resolve({data: 'Successful response from the API' });
+	});
+}
+module.exports = getPaymentTokenFromAPI;
