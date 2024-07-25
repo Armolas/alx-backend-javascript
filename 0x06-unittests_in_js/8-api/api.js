0ae9b24efb26841891ed3d9c0977aca07c4ca28c
Armolas <armolas06@gmail.com>
2024-07-25 23:31:43 +0100

    task_8

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -3,9 +3,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.status(200);
-	res.send('Welcome to the payment system');
+  res.status(200);
+  res.send('Welcome to the payment system');
 });
+
+module.exports = app;
 app.listen(7865, 'localhost', () => {
-	console.log('API available on localhost port 7865');
+  console.log('API available on localhost port 7865');
 });
--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,28 +1,18 @@
 const request = require('request');
 const { expect } = require('chai');
-const app = require('./api');
-const sinon = require('sinon');
 
-describe('Index Page', () => {
-	let spy = sinon.spy(console, 'log');
-
+describe('index Page', () => {
   it('should return status code 200', (done) => {
     request('http://localhost:7865/', (error, response, body) => {
-			expect(response.statusCode).to.equal(200);
-			done();
-		});
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
   });
 
   it('should return the correct message', (done) => {
     request('http://localhost:7865/', (error, response, body) => {
       expect(body).to.equal('Welcome to the payment system');
       done();
-		});
+    });
   });
-
-	it('should print API available on localhost port 7865 on start of server', (done) => {
-		expect(spy.calledWith('API available on localhost port 7865')).to.be.true;
-		spy.restore();
-		done();
-	});
 });
